6b7bb7916bfe091e792c90c6c22841e27ef10d27
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-11-22 20:04:47 -0300

    Add: metodos da Localização;

--- Localizacao.cpp
+++ Localizacao.cpp
@@ -1,1 +1,21 @@
 #include "Localizacao.h"
+
+Localizacao::Localizacao()
+{
+	this->x = 0;
+	this->y = 0;
+}
+
+Localizacao::~Localizacao()
+{
+}
+
+void Localizacao::SetX(int x)
+{
+	this->x = x;
+}
+
+void Localizacao::SetY(int y)
+{
+	this->y = y;
+}
--- Localizacao.h
+++ Localizacao.h
@@ -4,5 +4,10 @@ class Localizacao
 	private:
 		int x;
 		int y;
+	public:
+		Localizacao();
+		~Localizacao();
+		void SetX(int x);
+		void SetY(int y);
 };
 
